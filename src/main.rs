a3df8113abaeeffcb565accb4d8273f9db81e538
BPhilagor <benjamin.philagor@gmail.com>
2023-12-16 11:40:17 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use bevy::{prelude::*, input::mouse::MouseMotion, window::CursorGrabMode, pbr::DirectionalLightShadowMap, sprite::MaterialMesh2dBundle};
+use bevy::{prelude::*, input::mouse::MouseMotion, window::CursorGrabMode, pbr::DirectionalLightShadowMap};
 mod map;
 use map::*;
 fn main() {
     App::new()
     .add_plugins((DefaultPlugins, MapPlugin))
-    .add_systems(Update, (cursor_grab_system))
+    .add_systems(Update, (cursor_grab_system, move_camera))
     .add_systems(Startup, setup)
     .insert_resource(DirectionalLightShadowMap { size: 1000 })
     .run();
@@ -56,35 +56,21 @@ fn cursor_grab_system(
 fn move_camera(
     mut cam_query: Query<&mut Transform, With<Camera>>,
     key_inputs: Res<Input<KeyCode>>,
-    mut mouse_motion: EventReader<MouseMotion>,
     time: Res<Time>,
-    window: Query<&Window>,
 ) {
     //Position
     let mut camera_pos = cam_query.single_mut();
-    const SPEED: f32 = 100.;
-    const ROTATION_SPEED: f32 = 3.;
+    const SPEED: f32 = 1000.;
     
     let boost = if key_inputs.pressed(KeyCode::ControlLeft) {100.} else {1.};
     let move_value_x =  f32::from(key_inputs.pressed(KeyCode::D)) - f32::from(key_inputs.pressed(KeyCode::A));
-    let move_value_y =  f32::from(key_inputs.pressed(KeyCode::Space)) - f32::from(key_inputs.pressed(KeyCode::ShiftLeft)) ;
-    let move_value_z =  f32::from(key_inputs.pressed(KeyCode::S)) - f32::from(key_inputs.pressed(KeyCode::W));
+    let move_value_y =  f32::from(key_inputs.pressed(KeyCode::W)) - f32::from(key_inputs.pressed(KeyCode::S));
+    let move_value_z =  f32::from(key_inputs.pressed(KeyCode::Space)) - f32::from(key_inputs.pressed(KeyCode::ShiftLeft)) ;
 
-    let z_rotation = Quat::from_xyzw(0., camera_pos.rotation.y, 0., camera_pos.rotation.w).normalize();
-    let direction = z_rotation.mul_vec3(Vec3 { x: move_value_x, y: move_value_y, z: move_value_z }).normalize_or_zero();
+    let movement = Vec3 {x: move_value_x, y: move_value_y, z: move_value_z};
+    camera_pos.translation += movement * time.delta_seconds() * SPEED * boost;
 
-    camera_pos.translation += Vec3 {x: move_value_x, y: move_value_y, z: move_value_z} * time.delta_seconds() * SPEED * boost;
-
-    //Rotation
-    let mut rotation_vec = Vec2::ZERO;
-    for ev in mouse_motion.read() {
-        rotation_vec += ev.delta;
-    }
-
-    let window = window.single();
-    camera_pos.rotate_local_x(-rotation_vec.y / window.height() * ROTATION_SPEED);
-    camera_pos.rotate_y(-rotation_vec.x / window.width() * ROTATION_SPEED);
-    if direction != Vec3::ZERO {
+    if movement != Vec3::ZERO {
         println!("{:?}", camera_pos.translation);
     }
 }
