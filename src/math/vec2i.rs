a9deeffdf8740b25ac10879bfdb358ed9eec7a4d
gymnopediese <kalityoflife@gmail.com>
2023-12-16 23:14:26 +0100

    Wesh les plaque de pression elle sont un de ces tarpe

--- src/math/vec2i.rs
+++ src/math/vec2i.rs
@@ -1,3 +1,4 @@
+use bevy::math::Vec3;
 use serde::Deserialize;
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Default, Deserialize, Debug)]
@@ -9,5 +10,12 @@ pub struct Vec2i {
 impl Vec2i {
     pub fn from(x: i32, y: i32) -> Vec2i {
         Vec2i { x, y }
+    }
+
+    pub fn from_vec3(vec: Vec3) -> Vec2i {
+        Vec2i {
+            x: vec.x as i32,
+            y: vec.y as i32,
+        }
     }
 }
