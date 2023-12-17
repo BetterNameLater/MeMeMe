a60abfd02e2f8eb9477b18c20666c4c7d59ba3f6
gymnopediese <kalityoflife@gmail.com>
2023-12-17 16:37:04 +0100

    saluyt

--- src/items/people_on.rs
+++ src/items/people_on.rs
@@ -7,13 +7,16 @@ use crate::player::player::Player;
 use crate::player::{Ghost, GhostNewPositionEvent, PlayerNewPositionEvent};
 use bevy::prelude::*;
 
+use super::is_activated::{IsActivated, self};
+use super::is_usable::IsUsable;
+
 /// The number of [`Ghost`] and [`Player`] on this position
 #[derive(Component)]
 pub struct PeopleOn(pub usize);
 
 pub fn count_people_on_system<W: Component, E: NewPositionEvent>(
     mut player_new_position_event: EventReader<E>,
-    mut player_only_people_on_query: Query<&mut PeopleOn, (With<IsUsable>, Without<W>)>,
+    mut player_only_people_on_query: Query<(&mut IsActivated, &mut PeopleOn), (With<IsUsable>, Without<W>)>,
     mut object_map_query: Query<&Map, With<ObjectMap>>,
 ) {
     // TODO le system ne devait pas ce lancé tant que la map n'est pas lancée
@@ -25,14 +28,16 @@ pub fn count_people_on_system<W: Component, E: NewPositionEvent>(
         if let Some(leaved_cell) =
             object_map.get_cell_entity_by_pos(&new_position_event.get_before())
         {
-            if let Ok(mut people_on) = player_only_people_on_query.get_mut(leaved_cell) {
+            if let Ok((mut is_activated, mut people_on)) = player_only_people_on_query.get_mut(leaved_cell) {
                 people_on.0 -= 1;
+				is_activated.0 = people_on.0 > 0;
             }
         }
         if let Some(entered_cell) = object_map.get_cell_entity_by_pos(&new_position_event.get_now())
         {
-            if let Ok(mut people_on) = player_only_people_on_query.get_mut(entered_cell) {
+            if let Ok((mut is_activated, mut people_on)) = player_only_people_on_query.get_mut(entered_cell) {
                 people_on.0 += 1;
+				is_activated.0 = people_on.0 > 0;
             }
         }
     }
