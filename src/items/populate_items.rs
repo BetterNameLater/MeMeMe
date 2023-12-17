b06c3de3342e97c0f8b079427835d2ae7a7558bc
gymnopediese <kalityoflife@gmail.com>
2023-12-17 12:50:54 +0100

    object populate

---
+++ src/items/populate_items.rs
@@ -0,0 +1,48 @@
+use crate::constantes::{CELL_LENGTH, PLAYER_START_TRANSFORM};
+use crate::items::ghost_only::GhostOnly;
+use crate::items::is_on::IsOn;
+use crate::items::people_on::PeopleOn;
+use crate::items::player_only::PlayerOnly;
+use crate::map_parser::map_repr::{ObjectRepr, ObjectType};
+use crate::math::vec2i::Vec2i;
+use bevy::prelude::*;
+use bevy::utils::HashMap;
+
+use super::player_only::SingleUse;
+
+#[derive(Component)]
+pub struct Item;
+
+
+
+pub fn populate_items(mut commands: Commands, objects: &HashMap<String, ObjectRepr>)
+	-> HashMap<Vec2i, Entity>
+{
+
+	let mut items: HashMap<Vec2i, Entity> = HashMap::default();
+
+	for (key, object) in objects.iter() {
+
+
+		let item = commands.spawn(Item).id();
+		items.insert(object.position.into(), item.clone());
+
+		match object.object_type {
+			ObjectType::PressurePlate => {
+				commands.entity(item).insert(PeopleOn(0));
+				commands.entity(item).insert(IsOn(false));
+			},
+			_ => {}	
+		}
+		
+		if (object.ghost_only) {
+			commands.entity(item).insert(GhostOnly);
+		} else if (object.player_only) {
+			commands.entity(item).insert(PlayerOnly);
+		} else if (object.single_use) {
+			commands.entity(item).insert(SingleUse);
+		}
+
+	}
+	return items;
+}
--- src/items/pressure_plate.rs
+++ src/items/pressure_plate.rs
@@ -1,44 +1,44 @@
-use crate::constantes::{CELL_LENGTH, PLAYER_START_TRANSFORM};
-use crate::items::ghost_only::GhostOnly;
-use crate::items::is_on::IsOn;
-use crate::items::people_on::PeopleOn;
-use crate::items::player_only::PlayerOnly;
-use crate::map_parser::map_repr::{ObjectRepr, ObjectType};
-use crate::math::vec2i::Vec2i;
-use bevy::prelude::*;
-use bevy::utils::HashMap;
+// use crate::constantes::{CELL_LENGTH, PLAYER_START_TRANSFORM};
+// use crate::items::ghost_only::GhostOnly;
+// use crate::items::is_on::IsOn;
+// use crate::items::people_on::PeopleOn;
+// use crate::items::player_only::PlayerOnly;
+// use crate::map_parser::map_repr::{ObjectRepr, ObjectType};
+// use crate::math::vec2i::Vec2i;
+// use bevy::prelude::*;
+// use bevy::utils::HashMap;
 
-use super::player_only::SingleUse;
+// use super::player_only::SingleUse;
+
+// // #[derive(Component)]
+// // pub struct PressurePlate {
+// //     pub level: usize,
+// // }
+// //
 
 // #[derive(Component)]
-// pub struct PressurePlate {
-//     pub level: usize,
-// }
-//
+// pub struct Item;
 
-#[derive(Component)]
-pub struct Item;
+// pub fn spawn_pressure_plate(mut commands: Commands, objects: HashMap<String, ObjectRepr>) {
 
-pub fn spawn_pressure_plate(mut commands: Commands, objects: HashMap<String, ObjectRepr>) {
+// 	for (key, object) in objects.iter() {
+// 		let item = commands.spawn(Item).id();
 
-	for (key, object) in objects.iter() {
-		let item = commands.spawn(Item).id();
-
-		match object.object_type {
-			ObjectType::PressurePlate => {
-				commands.entity(item).insert(PeopleOn(0));
-				commands.entity(item).insert(IsOn(false));
-			},
-			_ => {}	
-		}
+// 		match object.object_type {
+// 			ObjectType::PressurePlate => {
+// 				commands.entity(item).insert(PeopleOn(0));
+// 				commands.entity(item).insert(IsOn(false));
+// 			},
+// 			_ => {}	
+// 		}
 		
-		if (object.ghost_only) {
-			commands.entity(item).insert(GhostOnly);
-		} else if (object.player_only) {
-			commands.entity(item).insert(PlayerOnly);
-		} else if (object.single_use) {
-			commands.entity(item).insert(SingleUse);
-		}
+// 		if (object.ghost_only) {
+// 			commands.entity(item).insert(GhostOnly);
+// 		} else if (object.player_only) {
+// 			commands.entity(item).insert(PlayerOnly);
+// 		} else if (object.single_use) {
+// 			commands.entity(item).insert(SingleUse);
+// 		}
 
-	}
-}
+// 	}
+// }
