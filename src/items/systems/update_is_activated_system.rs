88c458eddda2f6dd2c3946ab9a3b77bfda9978f4
quentin jungo <quentin.jungo@gmail.com>
2024-01-27 18:09:31 +0100

    refactor: simplify is_activated system

--- src/items/systems/update_is_activated_system.rs
+++ src/items/systems/update_is_activated_system.rs
@@ -5,19 +5,17 @@ use bevy::prelude::*;
 
 pub fn update_is_usable_system(
     mut commands: Commands,
-    is_activated_query: Query<(Entity, &IsActivated)>,
-    is_usable_and_dependencies_query: Query<(Entity, &Dependencies<On>, Option<&IsUsable>)>,
+    entity_is_activated_query: Query<(Entity, &IsActivated)>,
+    entity_dependencies_query: Query<(Entity, &Dependencies<On>)>,
 ) {
-    for (is_usable_entity, is_usable_dependencies, is_usable) in &is_usable_and_dependencies_query {
-        let my_dependencies_deactivated = is_activated_query
+    for (item, is_usable_dependencies) in &entity_dependencies_query {
+        let one_is_not_activated = entity_is_activated_query
             .iter()
-            .filter(|(id, is_activated)| is_usable_dependencies.0.contains(id) && !is_activated.0);
-        // all are activated
-        if my_dependencies_deactivated.count() == 0 && is_usable.is_none() {
-            commands.entity(is_usable_entity).insert(IsUsable);
-        // one is unactivated
+            .any(|(id, is_activated)| is_usable_dependencies.0.contains(&id) && !is_activated.0);
+        if one_is_not_activated {
+            commands.entity(item).remove::<IsUsable>();
         } else {
-            commands.entity(is_usable_entity).remove::<IsUsable>();
+            commands.entity(item).insert(IsUsable);
         }
     }
 }
@@ -28,28 +26,23 @@ pub fn update_is_unusable_system(
     is_usable_and_dependencies_query: Query<(
         Entity,
         &Dependencies<Off>,
-        Option<&IsUsable>,
         Option<&Dependencies<On>>,
     )>,
 ) {
-    for (is_usable_entity, is_unusable_dependencies, is_usable, u_deps) in
+    for (is_usable_entity, is_unusable_dependencies, on_dependencies) in
         &is_usable_and_dependencies_query
     {
-        let my_dependencies_activated = is_activated_query
+        let has_on_dependencies = on_dependencies.is_some();
+        let one_is_activated = is_activated_query
             .iter()
-            .filter(|(id, is_activated)| is_unusable_dependencies.0.contains(id) && is_activated.0);
-        let count = my_dependencies_activated.count();
-
-        // si ça ne depend pas de Dependencies<On> -> je fais mon histoire tout seul
-        if u_deps.is_none() {
-            // all are deactivated
-            if count == 0 && is_usable.is_none() {
-                commands.entity(is_usable_entity).insert(IsUsable);
-                // one is activated
-            } else {
+            .any(|(id, is_activated)| is_unusable_dependencies.0.contains(&id) && is_activated.0);
+        if !has_on_dependencies {
+            if one_is_activated {
                 commands.entity(is_usable_entity).remove::<IsUsable>();
+            } else {
+                commands.entity(is_usable_entity).insert(IsUsable);
             }
-        } else if count != 0 {
+        } else if one_is_activated {
             // si update_is_usable_system a ajouté `IsUsable`
             // on peut l'enlever
             commands.entity(is_usable_entity).remove::<IsUsable>();
