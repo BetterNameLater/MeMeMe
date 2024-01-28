a492d9b3343c37adfd70c0ad8c4374afccfa1dbb
quentin jungo <quentin.jungo@gmail.com>
2024-01-28 16:51:05 +0100

    fix: reset items state

--- src/player/systems/rewind_system.rs
+++ src/player/systems/rewind_system.rs
@@ -1,4 +1,7 @@
 use crate::constantes::PLAYER_Z;
+use crate::items::bundle::item_bundle::Item;
+use crate::items::components::is_activated::IsActivated;
+use crate::items::components::is_usable::IsUsable;
 use crate::level::components::level_tag::LevelTag;
 use crate::level::ressources::level_informations::LevelInformations;
 use crate::player::components::player::Player;
@@ -12,6 +15,7 @@ pub fn rewind_system(
     mut player_query: Query<(Entity, &mut Player, &mut Transform, &mut Name)>,
     mut ghost_transform_query: Query<&mut Transform, (Without<Player>, With<Ghost>)>,
     level_query: Query<Entity, With<LevelTag>>,
+    mut is_activated_query: Query<&mut IsActivated>,
     mut level_infos: ResMut<LevelInformations>,
     mut ghost_actions: ResMut<GhostActions>,
     mut rewind_event: EventReader<RewindEvent>,
@@ -58,4 +62,11 @@ pub fn rewind_system(
     });
 
     level_infos.rewind();
+
+    // reset items state
+    for mut item in &mut is_activated_query {
+        // TODO check if the player is on a pressure plate
+        // TODO check if a lever is set to `true` from the beginning ?
+        item.0 = false;
+    }
 }
