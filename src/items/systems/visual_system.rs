f30f55c4118fc423cdd6b3ca3947e03cef4c4e18
quentin jungo <quentin.jungo@gmail.com>
2024-01-28 16:33:20 +0100

    refactor: simplify visual_system

--- src/items/systems/visual_system.rs
+++ src/items/systems/visual_system.rs
@@ -4,30 +4,28 @@ use crate::items::components::is_usable::IsUsable;
 use bevy::prelude::*;
 
 pub fn visual_system(
-    mut items_query: Query<(Entity, Option<&IsUsable>, Option<&IsActivated>), With<Item>>,
+    mut items_query: Query<(Option<&IsUsable>, Option<&IsActivated>), With<Item>>,
     mut outline_query: Query<(&ItemOutline, &mut Sprite)>,
 ) {
-    for (item, is_usable, is_activated) in &mut items_query {
-        for (outline, mut outline_sprite) in &mut outline_query {
-            if outline.0 == item {
-                match outline.1 {
-                    OutlineType::IsUsable => {
-                        if is_usable.is_some() {
-                            outline_sprite.color = Color::GREEN;
-                        } else {
-                            outline_sprite.color = Color::RED;
-                        }
+    for (outline, mut outline_sprite) in &mut outline_query {
+        if let Ok((is_usable, is_activated)) = items_query.get_mut(outline.0) {
+            match outline.1 {
+                OutlineType::IsUsable => {
+                    if is_usable.is_some() {
+                        outline_sprite.color = Color::GREEN;
+                    } else {
+                        outline_sprite.color = Color::RED;
                     }
-                    OutlineType::IsActivated => {
-                        if let Some(is_activated) = is_activated {
-                            if is_activated.0 {
-                                outline_sprite.color = Color::MIDNIGHT_BLUE;
-                            } else {
-                                outline_sprite.color = Color::GRAY;
-                            }
+                }
+                OutlineType::IsActivated => {
+                    if let Some(is_activated) = is_activated {
+                        if is_activated.0 {
+                            outline_sprite.color = Color::MIDNIGHT_BLUE;
                         } else {
-                            outline_sprite.color = Color::WHITE;
+                            outline_sprite.color = Color::GRAY;
                         }
+                    } else {
+                        outline_sprite.custom_size = Some(Vec2::default());
                     }
                 }
             }
