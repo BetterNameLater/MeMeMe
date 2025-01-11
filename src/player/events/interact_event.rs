e2a31aa830f45146e7e4aeea91cb738440b58551
quentin jungo <quentin.jungo@gmail.com>
2025-01-11 13:34:41 +0100

    refactor: explicit interact-event marker as person

--- src/player/events/interact_event.rs
+++ src/player/events/interact_event.rs
@@ -1,22 +1,22 @@
-use crate::math::vec2i::Vec2i;
+use crate::{math::vec2i::Vec2i, player::components::person::Person};
 use bevy::prelude::*;
 use std::marker::PhantomData;
 
 #[derive(Event, Debug)]
-pub struct InteractEvent<T> {
+pub struct InteractEvent<T: Person> {
     pub _pos: Vec2i,
     pub person: Entity,
     pub item: Entity,
-    pub marker: PhantomData<T>,
+    pub person_marker: PhantomData<T>,
 }
 
-impl<T> InteractEvent<T> {
+impl<T: Person> InteractEvent<T> {
     pub fn new(pos: Vec2i, person: Entity, item: Entity) -> Self {
         Self {
             _pos: pos,
             person,
             item,
-            marker: Default::default(),
+            person_marker: PhantomData,
         }
     }
 }
